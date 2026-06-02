3a090f4d14e41293a43f24d02addc7270b4b2718
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-06-03 01:20:52 +0800

    Update NoiseThreaderPlugin.java

--- src/main/java/noisethreader/NoiseThreaderPlugin.java
+++ src/main/java/noisethreader/NoiseThreaderPlugin.java
@@ -1,10 +1,8 @@
 package noisethreader;
 
-import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import zone.rong.mixinbooter.IEarlyMixinLoader;
-import zone.rong.mixinbooter.ILateMixinLoader;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
@@ -13,37 +11,17 @@ import java.util.Map;
 
 @IFMLLoadingPlugin.MCVersion("1.12.2")
 @IFMLLoadingPlugin.SortingIndex(1001)
-public class NoiseThreaderPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader, ILateMixinLoader {
+public class NoiseThreaderPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
     public NoiseThreaderPlugin() {
         MixinBootstrap.init();
     }
 
-    // 早期 Mixin：不依赖其他模组，可以早期加载
+    // 早期 Mixin：不依赖其他模组
     @Override
     public List<String> getMixinConfigs() {
         List<String> configs = new ArrayList<>();
         configs.add("mixins.noisethreader.vanilla.json");
-        return configs;
-    }
-
-    // 晚期 Mixin：依赖其他模组，等模组加载完成后再加载
-    @Override
-    public List<String> getLateMixinConfigs() {
-        List<String> configs = new ArrayList<>();
-
-        if (Loader.isModLoaded("bettercaves")) {
-            configs.add("mixins.noisethreader.vanilla.cache.json");
-            configs.add("mixins.noisethreader.bettercaves.json");
-        }
-
-        if (Loader.isModLoaded("openterraingenerator")) {
-            configs.add("mixins.noisethreader.otg.json");
-            if (Loader.isModLoaded("bettercaves")) {
-                configs.add("mixins.noisethreader.otg.cache.json");
-            }
-        }
-
         return configs;
     }
 
